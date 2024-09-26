d2d4979f94e9825043af48aef79b443b9885bd02
zoska_instagram <alexhombauer@s.zochova.sk>
2024-09-26 10:24:53 +0200

    basic routing added

---
+++ src/app/profil/page.tsx
@@ -0,0 +1,15 @@
+
+// src/app/profil/page.tsx
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: `Zoznam profilov | zoska-instagram` };
+
+export default function ProfileList() {
+  
+  return (
+
+      <Typography> Zoznam profilov </Typography>
+
+  );
+}
